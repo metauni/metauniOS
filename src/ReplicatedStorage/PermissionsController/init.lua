0e4fb724617e20ed48949a2108115b0e737410be
Billy Snikkers <billyprice98is@gmail.com>
2023-01-22 22:13:02 +0100

    Fixed SystemChannel and topbar offset

--- src/ReplicatedStorage/PermissionsController/init.lua
+++ src/ReplicatedStorage/PermissionsController/init.lua
@@ -1,10 +1,11 @@
 local TextChatService = game:GetService("TextChatService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local PermissionsController = {}
 
 function PermissionsController:Start()
 
-	local SystemChannel: TextChannel = TextChatService:WaitForChild("RBXSystem", true)
+	local SystemChannel: TextChannel = TextChatService:WaitForChild("TextChannels"):WaitForChild("RBXSystem", true)
 
 	SystemChannel.OnIncomingMessage = function(textChatMessage: TextChatMessage)
 		local hexColor = textChatMessage.Metadata:match("^#?%x%x%x%x%x%x$")
@@ -19,6 +20,11 @@ function PermissionsController:Start()
 		local metadata = color and "#"..color:ToHex():upper() or "#FDE541"
 		SystemChannel:DisplaySystemMessage(message, metadata)
 	end)
+
+	do
+		local IconController = require(ReplicatedStorage.Icon.IconController)
+		IconController.setLeftOffset(12 + 32)
+	end
 end
 
 return PermissionsController
